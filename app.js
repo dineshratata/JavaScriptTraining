2124271ed22c41c4028da2becfbb79e507b148ae
dineshratata <dineshstallone15196@gmail.com>
2024-03-22 12:34:17 +0530

    First

---
+++ app.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('#Form');
+const UserName = document.querySelector('#UserName');
+const Email = document.querySelector('#Email');
+const PassWord = document.querySelector('#PassWord');
+const ConfirmPassWord = document.querySelector('#ConfirmPassWord');
+
+
+
+form.addEventListener('submit' ,(e)=> {
+
+e.preventDefault();
+validateInput();
+
+} )
+
+function validateInput(){
+
+const usernameVal = UserName.ariaValueMax.trim()
+const emailVal = Email.validateInput
+
+
+
+}
+
+
